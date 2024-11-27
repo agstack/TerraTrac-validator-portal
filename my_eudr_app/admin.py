debb4114412ecf3366f2f8f6827426ce05b0c965
julesntare <julesntare@gmail.com>
2024-11-27 12:32:15 +0200

    added missing models to django admin portal list

--- my_eudr_app/admin.py
+++ my_eudr_app/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from eudr_backend.models import WhispAPISetting, EUDRFarmModel
+from eudr_backend.models import EUDRCollectionSiteModel, EUDRFarmBackupModel, EUDRSharedMapAccessCodeModel, EUDRUploadedFilesModel, EUDRUserModel, WhispAPISetting, EUDRFarmModel
 
 admin.site.register(
     [
+        EUDRUserModel,
         EUDRFarmModel,
+        EUDRUploadedFilesModel,
+        EUDRCollectionSiteModel,
+        EUDRFarmBackupModel,
+        EUDRSharedMapAccessCodeModel,
         WhispAPISetting
     ]
 )
